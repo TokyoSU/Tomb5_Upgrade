0e49706e53b5385173b3d6efcf1f343e95073fa1
TokyoSU <77746747+TokyoSU@users.noreply.github.com>
2024-09-07 15:25:46 +0200

    First version

---
+++ compile_vcpkg_libs.bat
@@ -0,0 +1,10 @@
+@echo off
+@title Compiling vcpkg libraries
+@color 0
+vcpkg install glm:x64-windows
+vcpkg install glfw3:x64-windows
+vcpkg install glad[core,loader,extensions,gl-api-latest]:x64-windows
+vcpkg install assimp:x64-windows
+vcpkg install tinyfiledialogs:x64-windows
+vcpkg install spdlog:x64-windows
+pause
